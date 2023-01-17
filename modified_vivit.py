41136e399a4360e40293dd208e84bca563ee87c0
Aaron Jimenez <arjl5012@gmail.com>
2023-01-17 15:42:47 -0800

    Added model 2 for ViVit

--- modified_vivit.py
+++ modified_vivit.py
@@ -4,13 +4,43 @@ import re
 import numpy as np
 
 import warnings
-from utils import print_on_rank_zero
+#from utils import print_on_rank_zero
 
 from einops import rearrange, reduce, repeat
 
 from torch import nn
 
 from transformer_components import TransformerContainer
+
+from pytorch_lightning.utilities.distributed import rank_zero_only
+import torch.distributed as dist
+
+
+@rank_zero_only
+def print_on_rank_zero(content):
+	if is_main_process():	
+		print(content)
+
+def is_dist_avail_and_initialized():
+	if not dist.is_available():
+		return False
+	if not dist.is_initialized():
+		return False
+	return True
+
+def get_world_size():
+	if not is_dist_avail_and_initialized():
+		return 1
+	return dist.get_world_size()
+
+def get_rank():
+	if not is_dist_avail_and_initialized():
+		return 0
+	return dist.get_rank()
+
+def is_main_process():
+	return get_rank() == 0
+
 
 
 def _pair(x):
@@ -113,6 +143,7 @@ def init_from_vit_pretrain_(module,
 			if conv_type == 'Conv3d':
 				if 'patch_embed.projection.weight' in old_key:
 					weight = state_dict[old_key]
+					print("*"*24, weight.shape)
 					new_weight = repeat(weight, 'd c h w -> d c t h w', t=tube_size)
 					if extend_strategy == 'temporal_avg':
 						new_weight = new_weight / tube_size
@@ -277,7 +308,7 @@ class PatchEmbed(nn.Module):
 		return x
 
 
-class ViViT(nn.Module):
+class ViViTModel3(nn.Module):
 	"""ViViT. A PyTorch impl of `ViViT: A Video Vision Transformer`
 		<https://arxiv.org/abs/2103.15691>
 	Args:
@@ -563,3 +594,203 @@ class ViViT(nn.Module):
 			print(x.shape)
 			x = temporal_transformer(x, return_attention=True)
 		return x
+	
+class ViViTModel2(nn.Module):
+    """ViViT. A PyTorch impl of `ViViT: A Video Vision Transformer`
+        
+
+    Args:
+        num_frames (int): Number of frames in the video.
+        img_size (int | tuple): Size of input image.
+        patch_size (int): Size of one patch.
+        pretrained (str | None): Name of pretrained model. Default: None.
+        embed_dims (int): Dimensions of embedding. Defaults to 768.
+        num_heads (int): Number of parallel attention heads. Defaults to 12.
+        num_transformer_layers (int): Number of transformer layers. Defaults to 12.
+        in_channels (int): Channel num of input features. Defaults to 3.
+        dropout_p (float): Probability of dropout layer. Defaults to 0..
+        tube_size (int): Dimension of the kernel size in Conv3d. Defaults to 2.
+        conv_type (str): Type of the convolution in PatchEmbed layer. Defaults to Conv3d.
+        attention_type (str): Type of attentions in TransformerCoder. Choices
+            are 'divided_space_time', 'fact_encoder' and 'joint_space_time'.
+            Defaults to 'fact_encoder'.
+        norm_layer (dict): Config for norm layers. Defaults to nn.LayerNorm.
+        copy_strategy (str): Copy or Initial to zero towards the new additional layer.
+        extend_strategy (str): How to initialize the weights of Conv3d from pre-trained Conv2d.
+        use_learnable_pos_emb (bool): Whether to use learnable position embeddings.
+        return_cls_token (bool): Whether to use cls_token to predict class label.
+    """
+    supported_attention_types = [
+        'fact_encoder', 'joint_space_time', 'divided_space_time'
+    ]
+
+    def __init__(self,
+                 num_frames,
+                 img_size,
+                 patch_size,
+                 embed_dims=768,
+                 num_heads=12,
+                 num_transformer_layers=12,
+                 in_channels=3,
+                 dropout_p=0.,
+                 tube_size=2,
+                 conv_type='Conv3d',
+                 attention_type='fact_encoder',
+                 norm_layer=nn.LayerNorm,
+                 return_cls_token=True,
+                 **kwargs):
+        super().__init__()
+        assert attention_type in self.supported_attention_types, (
+            f'Unsupported Attention Type {attention_type}!')
+
+        num_frames = num_frames//tube_size
+        self.num_frames = num_frames
+        self.embed_dims = embed_dims
+        self.num_transformer_layers = num_transformer_layers
+        self.attention_type = attention_type
+        self.conv_type = conv_type
+        self.tube_size = tube_size
+        self.num_time_transformer_layers = 4
+        self.return_cls_token = return_cls_token
+
+        #tokenize & position embedding
+        self.patch_embed = PatchEmbed(
+            img_size=img_size,
+            patch_size=patch_size,
+            in_channels=in_channels,
+            embed_dims=embed_dims,
+            tube_size=tube_size,
+            conv_type=conv_type)
+        num_patches = self.patch_embed.num_patches
+
+        # Divided Space Time Transformer Encoder - Model 2
+        transformer_layers = nn.ModuleList([])
+
+        spatial_transformer = TransformerContainer(
+            num_transformer_layers=num_transformer_layers,
+            embed_dims=embed_dims,
+            num_heads=num_heads,
+            num_frames=num_frames,
+            norm_layer=norm_layer,
+            hidden_channels=embed_dims*4,
+            operator_order=['self_attn','ffn'])
+
+        temporal_transformer = TransformerContainer(
+            num_transformer_layers=self.num_time_transformer_layers,
+            embed_dims=embed_dims,
+            num_heads=num_heads,
+            num_frames=num_frames,
+            norm_layer=norm_layer,
+            hidden_channels=embed_dims*4,
+            operator_order=['self_attn','ffn'])
+
+        transformer_layers.append(spatial_transformer)
+        transformer_layers.append(temporal_transformer)
+
+        self.transformer_layers = transformer_layers
+        self.norm = norm_layer(embed_dims, eps=1e-6)
+
+        self.cls_token = nn.Parameter(torch.zeros(1,1,embed_dims))
+        # whether to add one cls_token in temporal pos_enb
+        num_frames = num_frames + 1
+        num_patches = num_patches + 1
+
+        self.pos_embed = nn.Parameter(torch.zeros(1,num_patches,embed_dims))
+        self.time_embed = nn.Parameter(torch.zeros(1,num_frames,embed_dims))
+        self.drop_after_pos = nn.Dropout(p=dropout_p)
+        self.drop_after_time = nn.Dropout(p=dropout_p)
+    
+    def interpolate_pos_encoding(self, x, w, h):
+        npatch = x.shape[1] - 1
+        N = self.pos_embed.shape[1] - 1
+        if npatch == N and w == h:
+            return self.pos_embed
+        class_pos_embed = self.pos_embed[:, 0]
+        patch_pos_embed = self.pos_embed[:, 1:]
+        dim = x.shape[-1]
+        w0 = w // self.patch_embed.patch_size[0]
+        h0 = h // self.patch_embed.patch_size[0]
+        # we add a small number to avoid floating point error in the interpolation
+        # see discussion at https://github.com/facebookresearch/dino/issues/8
+        w0, h0 = w0 + 0.1, h0 + 0.1
+        patch_pos_embed = nn.functional.interpolate(
+            patch_pos_embed.reshape(1, int(math.sqrt(N)), int(math.sqrt(N)), dim).permute(0, 3, 1, 2),
+            scale_factor=(w0 / math.sqrt(N), h0 / math.sqrt(N)),
+            mode='bicubic',
+        )
+        assert int(w0) == patch_pos_embed.shape[-2] and int(h0) == patch_pos_embed.shape[-1]
+        patch_pos_embed = patch_pos_embed.permute(0, 2, 3, 1).view(1, -1, dim)
+        return torch.cat((class_pos_embed.unsqueeze(0), patch_pos_embed), dim=1)
+
+    def forward(self, x):
+        #Tokenize
+        b, t, c, h, w = x.shape
+        x = self.patch_embed(x)
+
+        # Add Position Embedding
+        cls_tokens = repeat(self.cls_token, 'b ... -> (repeat b) ...', repeat=x.shape[0])
+        x = torch.cat((cls_tokens, x), dim=1)
+        print(x.shape)
+        x = x + self.interpolate_pos_encoding(x, w, h)
+        x = self.drop_after_pos(x)
+
+        # fact encoder - CRNN style
+        spatial_transformer, temporal_transformer, = *self.transformer_layers,
+        print(x.shape)
+        x = spatial_transformer(x)
+
+        print(x.shape)
+        # Add Time Embedding
+        cls_tokens = x[:b, 0, :].unsqueeze(1)
+        x = rearrange(x[:, 1:, :], '(b t) p d -> b t p d', b=b)
+        x = reduce(x, 'b t p d -> b t d', 'mean')
+        x = torch.cat((cls_tokens, x), dim=1)
+        print(x.shape)
+        print('Time:',self.time_embed.shape)
+        x = x + self.time_embed
+        x = self.drop_after_time(x)
+
+        x = temporal_transformer(x)
+
+        x = self.norm(x)
+        return x
+        # Return Class Token
+        if self.return_cls_token:
+            return x[:, 0]
+        else:
+            return x[:, 1:].mean(1)
+
+def replace_state_dict(state_dict):
+	for old_key in list(state_dict.keys()):
+		if old_key.startswith('model'):
+			new_key = old_key[6:]
+			state_dict[new_key] = state_dict.pop(old_key)
+		else:
+			new_key = old_key[9:]
+			state_dict[new_key] = state_dict.pop(old_key)
+
+def create_lookahead_mask(shape:int) -> torch.Tensor:
+    return torch.triu(torch.ones(shape, shape) * float('-inf'), diagonal=1)
+if __name__ == "__main__":
+	num_frames = 15
+	img_size = 256
+	inp = torch.zeros(1, num_frames, 3, img_size, img_size)
+	model = ViViTModel2(num_frames=num_frames,
+                img_size=img_size,
+                patch_size=8,
+                embed_dims=768,
+                in_channels=3,
+                attention_type='joint_space_time',
+                return_cls_token=False,
+				conv_type='Conv3d')
+	outpt = model(inp)
+	print(torch.squeeze(outpt).shape)
+	print(outpt[0][0].shape)
+	print(outpt[:,0][0].shape)
+	print(torch.equal(outpt[0][0], outpt[:,0][0]))
+	decoder_layer = nn.TransformerDecoderLayer(d_model=768, nhead=8)
+	decoder = nn.TransformerDecoder(decoder_layer=decoder_layer, num_layers=4)
+	mask = create_lookahead_mask(54)
+	tgt = torch.rand(54, 768)
+	print(decoder(tgt, outpt[0], tgt_mask=mask).shape)
+	#print(model(inp)[:].shape)
